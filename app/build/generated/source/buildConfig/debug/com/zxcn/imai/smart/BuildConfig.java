204e7f14c01b1aa84ec90fd5f0300a8243dc1acd
zxcn <437798373@qq.com>
2018-10-11 12:14:58 +0800

    取消蓝牙

---
+++ app/build/generated/source/buildConfig/debug/com/zxcn/imai/smart/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.zxcn.imai.smart;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.zxcn.imai.smart";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
---
+++ app/build/generated/source/buildConfig/release/com/zxcn/imai/smart/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.zxcn.imai.smart;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.zxcn.imai.smart";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
